61c8132216d86ee2e17725c84c8587f4b1fa177d
DanielvanLeuzen <Leuzen@hotmail.com>
2018-08-25 15:16:06 +0200

    update 1

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,19 @@
 import { combineReducers } from 'redux'
 import links from './links'
 import selectedLink from './selected-links'
+import { BrowserRouter} from 'react-router-dom'
+
+
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//   <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+
+// )
+
+
 
 export default combineReducers({
   links, 
